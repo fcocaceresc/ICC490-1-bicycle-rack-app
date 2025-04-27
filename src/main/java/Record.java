faf70a2c923c6bc19ed49bad9f6eac7df2ba0b2b
Francisco <fcocacerescolihuil@gmail.com>
2025-04-27 19:22:44 -0400

    add Record data class

---
+++ src/main/java/Record.java
@@ -0,0 +1,53 @@
+import java.time.LocalDateTime;
+
+public class Record {
+    private final int id;
+    private Student student;
+    private String bicycleDescription;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+
+    public Record(int id, Student student, String bicycleDescription) {
+        this.id = id;
+        this.student = student;
+        this.bicycleDescription = bicycleDescription;
+        this.checkIn = LocalDateTime.now();
+        this.checkOut = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getBicycleDescription() {
+        return bicycleDescription;
+    }
+
+    public void setBicycleDescription(String bicycleDescription) {
+        this.bicycleDescription = bicycleDescription;
+    }
+
+    public LocalDateTime getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDateTime checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDateTime getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDateTime checkOut) {
+        this.checkOut = checkOut;
+    }
+}
