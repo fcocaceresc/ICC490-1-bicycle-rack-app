641013d6aa5837ee814030edf4cf43193fad3b36
Francisco <fcocacerescolihuil@gmail.com>
2025-04-27 20:53:28 -0400

    add BicycleRackService class and check in operation

---
+++ src/main/java/BicycleRackService.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class BicycleRackService {
+    private final ArrayList<Record> records = new ArrayList<>();
+
+    public Record checkIn(Student student, String bicycleDescription) {
+        if (student.getId() == null || student.getId().isEmpty()) {
+            throw new IllegalArgumentException("Student ID can't be empty");
+        }
+
+        if (student.getName() == null || student.getName().isEmpty()) {
+            throw new IllegalArgumentException("Student name can't be empty");
+        }
+
+        if (bicycleDescription == null || bicycleDescription.isEmpty()) {
+            throw new IllegalArgumentException("Bicycle description can't be empty");
+        }
+
+        if (hasNotCheckedOutRecord(student.getId())) {
+            throw new IllegalStateException("The student has a not checked out record");
+        }
+
+        Record record = new Record(records.size() + 1, student, bicycleDescription);
+        records.add(record);
+        return record;
+    }
+
+    private boolean hasNotCheckedOutRecord(String studentId) {
+        for (Record record : records) {
+            if (record.getStudent().getId().equals(studentId) && record.getCheckOut() == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
