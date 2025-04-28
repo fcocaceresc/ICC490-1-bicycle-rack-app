4bafdf83defbe630c642392e7c9c811f6e203b2e
Francisco <fcocacerescolihuil@gmail.com>
2025-04-27 20:58:18 -0400

    separate check in input validation into another method

--- src/main/java/BicycleRackService.java
+++ src/main/java/BicycleRackService.java
@@ -4,25 +4,25 @@ public class BicycleRackService {
     private final ArrayList<Record> records = new ArrayList<>();
 
     public Record checkIn(Student student, String bicycleDescription) {
-        if (student.getId() == null || student.getId().isEmpty()) {
-            throw new IllegalArgumentException("Student ID can't be empty");
-        }
-
-        if (student.getName() == null || student.getName().isEmpty()) {
-            throw new IllegalArgumentException("Student name can't be empty");
-        }
-
-        if (bicycleDescription == null || bicycleDescription.isEmpty()) {
-            throw new IllegalArgumentException("Bicycle description can't be empty");
-        }
-
-        if (hasNotCheckedOutRecord(student.getId())) {
-            throw new IllegalStateException("The student has a not checked out record");
-        }
-
+        validateCheckIn(student, bicycleDescription);
         Record record = new Record(records.size() + 1, student, bicycleDescription);
         records.add(record);
         return record;
+    }
+
+    private void validateCheckIn(Student student, String bicycleDescription) {
+        if (student.getId() == null || student.getId().isEmpty()) {
+            throw new IllegalArgumentException("Student ID can't be empty");
+        }
+        if (student.getName() == null || student.getName().isEmpty()) {
+            throw new IllegalArgumentException("Student name can't be empty");
+        }
+        if (bicycleDescription == null || bicycleDescription.isEmpty()) {
+            throw new IllegalArgumentException("Bicycle description can't be empty");
+        }
+        if (hasNotCheckedOutRecord(student.getId())) {
+            throw new IllegalStateException("The student has a not checked out record");
+        }
     }
 
     private boolean hasNotCheckedOutRecord(String studentId) {
