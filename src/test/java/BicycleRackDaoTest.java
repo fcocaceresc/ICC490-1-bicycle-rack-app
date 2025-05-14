1fccf0c41e664c8d03dd1ea7cd0e9baed757e4d2
Francisco <fcocacerescolihuil@gmail.com>
2025-05-14 05:19:33 -0400

    add update record tests

--- src/test/java/BicycleRackDaoTest.java
+++ src/test/java/BicycleRackDaoTest.java
@@ -137,4 +137,101 @@ class BicycleRackDaoTest {
         Record record = bicycleRackDao.getRecords().getFirst();
         assertNotNull(record.getCheckOut());
     }
+
+    @Test
+    void updateRecord() {
+        bicycleRackDao.createRecord("4" , "amadeus", "oxford");
+        bicycleRackDao.updateRecord(1, "5", "salieri", "bmx");
+        ArrayList<Record> records = bicycleRackDao.getRecords();
+        Record record = records.getFirst();
+        Student student = record.getStudent();
+        assertEquals(1, record.getId());
+        assertEquals("5", student.getId());
+        assertEquals("salieri", student.getName());
+        assertEquals("bmx", record.getBicycleDescription());
+    }
+
+    @Test
+    void updateRecordNullStudentId() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, null, "salieri", "bmx");
+        });
+        assertEquals("The student id can't be null", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordEmptyStudentId() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "", "salieri", "bmx");
+        });
+        assertEquals("The student id can't be empty", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordNullStudentName() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "5", null, "bmx");
+        });
+        assertEquals("The student name can't be null", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordEmptyStudentName() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "5", "", "bmx");
+        });
+        assertEquals("The student name can't be empty", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordNullBicycleDescription() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "5", "salieri", null);
+        });
+        assertEquals("The bicycle description can't be null", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordEmptyBicycleDescription() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "5", "salieri", "");
+        });
+        assertEquals("The bicycle description can't be empty", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordStudentIdExceedsMaxLength() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        int studentIdMaxLength = ConfigReader.getIntValue("record.student.id.maxLength");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "a".repeat(studentIdMaxLength + 1), "salieri", "bmx");
+        });
+        assertEquals("The student id exceeds the maximum length of " + studentIdMaxLength + " characters", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordStudentNameExceedsMaxLength() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        int studentNameMaxLength = ConfigReader.getIntValue("record.student.name.maxLength");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "5", "a".repeat(studentNameMaxLength + 1), "bmx");
+        });
+        assertEquals("The student name exceeds the maximum length of " + studentNameMaxLength + " characters", exception.getMessage());
+    }
+
+    @Test
+    void updateRecordBicycleDescriptionExceedsMaxLength() {
+        bicycleRackDao.createRecord("4", "amadeus", "oxford");
+        int bicycleDescriptionMaxLength = ConfigReader.getIntValue("record.bicycleDescription.maxLength");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bicycleRackDao.updateRecord(1, "5", "salieri", "a".repeat(bicycleDescriptionMaxLength + 1));
+        });
+        assertEquals("The bicycle description exceeds the maximum length of " + bicycleDescriptionMaxLength + " characters", exception.getMessage());
+    }
 }
